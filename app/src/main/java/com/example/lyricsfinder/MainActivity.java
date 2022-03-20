bb59431990b0b669e50683bbd559c00abe3c9114
anshyadav <anshyadav4244@gmail.com>
2022-03-21 01:29:30 +0530

    Finised Project

--- app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lyricsfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
 
@@ -24,16 +35,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         edtArtistName = findViewById(R.id.edtArtistName);
         edtSongName = findViewById(R.id.edtSongName);
         findLyricsBtn = findViewById(R.id.findLyricsBtn);
         txtLyrcis = findViewById(R.id.lyricsTextView);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
 
         findLyricsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                URL url = "https://api.lyrics.ovh/v1/" +
+                if(TextUtils.isEmpty(edtArtistName.getText().toString()) || TextUtils.isEmpty(edtSongName.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Please Fill All Constraints", Toast.LENGTH_SHORT).show();
+                }
+                String url = "https://api.lyrics.ovh/v1/" +  edtArtistName.getText().toString() + "/" + edtSongName.getText().toString();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            txtLyrcis.setText(response.getString("lyrics"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
             }
         });
     }
